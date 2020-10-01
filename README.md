7b6ce0fcd343a1cd1f794a57f5a7437b08f47780
Kraig Taylor <tay15028@byui.edu>
2020-09-30 21:40:46 -0600

    implementation of main, and basic readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Meal-Randomizer
+
+This program makes it easy to plan meals for the week while keeping things fresh.
+
+To start randomizing, simply edit the menu template to add recipes that you like to cook and run the program. The Meal Randomizer will then create a file with the meal plan and shopping list for the week. To ensure variety, the Meal Randomizer will never schedule the same entree twice in one week.
---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "randomizer.h"
+using std::string;
+using std::cout;
+using std::cin;
+
+int main()
+{
+    string fileName;
+    int numMeals = 0;
+
+    cout << "Enter the name of the menu file: ";
+    cin >> fileName;
+    while (numMeals <= 0)
+    {
+        cout << "How many meals do you want to plan for? ";
+        cin >> numMeals;
+        if (numMeals <= 0) cout << "Please enter a number greater than 0.\n";
+    }
+    Randomizer r(fileName, numMeals);
+    r.initialize;
+    r.randomize;
+    r.printShoppingList;
+
+    return 0;
+}
