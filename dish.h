f7ec92a3c0bf4eb3fcb82a2c7a6e795cbd422282
Kraig Taylor <tay15028@byui.edu>
2020-10-01 16:48:37 -0600

    implemented the Dish extraction and insertion opps

--- dish.h
+++ dish.h
@@ -24,14 +24,33 @@ class Dish
     friend std::istream& operator>>(std::istream& in, Dish &d);
 };
 
+// Insertion operator: prints the name of the recipe, the ingredients, and closes the recipe with a '*'
 std::ostream& operator<<(std::ostream& out, const Dish &d)
 {
-    //TODO
+    out << d.name << "\n";
+    for (int i = 0; i < d.recipe.size(); i++)
+    {
+        out << "\t" << d.recipe[i];
+    }
+
+    out << "*\n";
+
+    return out;
 }
 
+// Extraction operator: extracts a recipe from the stream into the Dish object
 std::istream& operator>>(std::istream& in, Dish &d)
 {
-    //TODO
+    string line;
+    getline(in, line); // extract the title of the recipe
+    d.name = line;
+    while (in.peek() != '*') // extract ingredients until a '*' is encountered
+    {
+        getline(in, line);
+        d.recipe.push_back(line);
+    }
+    in.ignore(256, '\n');
+    return in;
 }
 
 
